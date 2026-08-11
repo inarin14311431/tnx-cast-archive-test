8646fd2a864e1805b4b496e9efae538dad1520c6
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-08-11 16:12:23 +0900

    Add master search enhancement entry module

---
+++ js/sheet-master-search-enhancements.js
@@ -0,0 +1,5 @@
+/* Master-search enhancement entry point.
+ * Loaded after the editor's deferred modules have initialized the search dialog. */
+import "./sheet-master-search-result-ui.js";
+import "./sheet-master-search-ofc-normalize.js";
+import "./sheet-master-search-bs-tooltips.js?v=2";
