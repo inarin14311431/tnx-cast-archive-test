ab92768e71a313729307df7fcb12f3317619c9fd
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-08-18 11:15:40 +0900

    Align mobile ability bootstrap with current UI

--- js/sheet-mobile-ability.js
+++ js/sheet-mobile-ability.js
@@ -1,4 +1,4 @@
-import "./sheet-mobile-ui.js?v=20260818-4";
+import "./sheet-mobile-ui.js?v=20260818-7";
 import "./sheet-mobile-profile.js?v=20260818-2";
 import "./sheet-mobile-style.js?v=20260818-2";
 import "./sheet-mobile-outfit.js?v=20260818-3";
