e584aeb78c75b8c00c71d49c24d4a156425081ef
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-08-18 17:07:29 +0900

    Refresh mobile editor shared-context modules

--- js/sheet-mobile-app.js
+++ js/sheet-mobile-app.js
@@ -11,5 +11,5 @@ import "./sheet-mobile-snapshots.js?v=20260818-2";
 import "./sheet-mobile-image.js?v=20260818-2";
 import "./sheet-mobile-import.js?v=20260818-4";
 import "./sheet-mobile-summary-text.js?v=20260818-3";
-import "./sheet-mobile-header-exp.js?v=20260818-3";
+import "./sheet-mobile-header-exp.js?v=20260818-4";
 import "./sheet-mobile-ui.js?v=20260818-11";
