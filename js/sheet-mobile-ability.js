10177d6497f4c93e900170af199ff4c0158b34b7
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-08-18 15:15:24 +0900

    Decouple mobile ability module from UI side effect imports

--- js/sheet-mobile-ability.js
+++ js/sheet-mobile-ability.js
@@ -1,7 +1,3 @@
-import "./sheet-mobile-ui.js?v=20260818-9";
-import "./sheet-mobile-profile.js?v=20260818-3";
-import "./sheet-mobile-style.js?v=20260818-3";
-import "./sheet-mobile-outfit.js?v=20260818-5";
 import { supabase } from "./supabase-client.js";
 import { requireAuth } from "./auth-state.js?v=4";
 import { STYLE_DATA, UTSUWA_ATTRIBUTES } from "./style-data.js";
