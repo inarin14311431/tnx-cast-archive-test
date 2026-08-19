68bae3db57a8f699361700a6877ff0412d693bee
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-08-19 11:57:24 +0900

    Bump mobile outfit UI grouping cache

--- js/sheet-mobile-outfit.js
+++ js/sheet-mobile-outfit.js
@@ -17,7 +17,7 @@ import {
   ensureOutfitStylesheet,
   ensureOutfitToolbar,
   renderOutfitCards
-} from "./sheet-mobile-outfit-ui.js?v=11";
+} from "./sheet-mobile-outfit-ui.js?v=12";
 
 const $ = selector => document.querySelector(selector);
 
