ef2aa3d1057239e216ef1397a15a87de0b66d224
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-08-20 22:28:47 +0900

    Delete temporary mobile CSS refresh helper

--- js/sheet-mobile-ordering-style-refresh.js
+++
@@ -1,9 +0,0 @@
-const versions = [
-  ["link[data-mobile-skills-style]", "./css-next/pages/sheet-mobile-skills.css?v=6"],
-  ["link[data-mobile-outfit-style]", "./css-next/pages/sheet-mobile-outfit.css?v=9"]
-];
-
-for (const [selector, href] of versions) {
-  const link = document.querySelector(selector);
-  if (link) link.href = href;
-}
