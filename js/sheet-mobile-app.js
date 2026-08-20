2390be13219dea2d96a4c6ebdb1213dc28ca4e72
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-08-20 14:15:41 +0900

    Load mobile starred skill display layer

--- js/sheet-mobile-app.js
+++ js/sheet-mobile-app.js
@@ -5,6 +5,7 @@ import "./sheet-mobile-profile.js?v=20260818-4";
 import "./sheet-mobile-style.js?v=20260818-4";
 import "./sheet-mobile-ability.js?v=20260818-1";
 import "./sheet-mobile-skills.js?v=20260818-4";
+import "./sheet-mobile-general-display.js?v=20260820-1";
 import "./sheet-mobile-style-existing-values.js?v=5";
 import "./sheet-mobile-outfit.js?v=20260819-6";
 import "./sheet-mobile-combos.js?v=20260818-7";
