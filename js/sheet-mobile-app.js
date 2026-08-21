1081a460bfa17d773f5da32226e812baa1abf7fe
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-08-21 09:15:10 +0900

    Bump mobile experience calculator cache

--- js/sheet-mobile-app.js
+++ js/sheet-mobile-app.js
@@ -14,5 +14,5 @@ import "./sheet-mobile-snapshots.js?v=20260818-3";
 import "./sheet-mobile-image.js?v=20260818-3";
 import "./sheet-mobile-import.js?v=20260818-4";
 import "./sheet-mobile-summary-text.js?v=20260818-3";
-import "./sheet-mobile-header-exp.js?v=20260820-3";
+import "./sheet-mobile-header-exp.js?v=20260821-1";
 import "./sheet-mobile-ui.js?v=20260818-11";
