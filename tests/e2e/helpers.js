15e341d7c636b41224f42f97e508c546b76b2ca6
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-08-24 14:46:26 +0900

    Add troop view browser regression coverage

--- tests/e2e/helpers.js
+++ tests/e2e/helpers.js
@@ -4,6 +4,10 @@ export function getTestCastId() {
   return process.env.E2E_CAST_ID || process.env.TEST_CAST_ID || "TNX-000091";
 }
 
+export function getTestTroopId() {
+  return process.env.E2E_TROOP_ID || process.env.TEST_TROOP_ID || "TRP-56F796299BEC";
+}
+
 export function hasAuthCredentials() {
   return Boolean(
     (process.env.E2E_EMAIL || process.env.TEST_EMAIL) &&
