81d6a1b6f5b103f9513a9814ff1fd0d15f6101df
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-09-10 07:54:47 +0900

    Refresh background preset module cache key

--- js/showcase-background-preset-picker.js
+++ js/showcase-background-preset-picker.js
@@ -1,7 +1,7 @@
 import {
   SHOWCASE_BACKGROUND_PRESETS,
   findShowcaseBackgroundPresetByUrl
-} from "./showcase-background-presets.js?v=3";
+} from "./showcase-background-presets.js?v=4";
 
 const grid = document.querySelector("#background-preset-grid");
 const keyField = document.querySelector("#background-preset");
