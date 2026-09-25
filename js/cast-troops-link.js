0800e6ee4364be5fb82272fd338fcfca9689c454
inarin14311431 <122023522+inarin14311431@users.noreply.github.com>
2026-09-25 15:47:46 +0900

    refactor: remove local escaping implementation from js/cast-troops-link.js

--- js/cast-troops-link.js
+++ js/cast-troops-link.js
@@ -182,4 +182,3 @@ function decorateExperience(expText, scope = "") {
 }
 
 function num(value) { return Math.max(0, Number(value) || 0); }
-function escapeHtml(value) { return String(value ?? "").replace(/[&<>"']/g, c => ({ "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&#39;" }[c])); }
